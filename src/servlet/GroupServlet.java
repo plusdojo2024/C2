5a43e77e52ae9bc44d817d7cc01ef88d9065d9a9
plusdojo2024-zama <zama-hidaka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-14 15:26:04 +0900

    Group

---
+++ src/servlet/GroupServlet.java
@@ -0,0 +1,76 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.GroupsDAO;
+
+/**
+ * Servlet implementation class GroupServlet
+ */
+@WebServlet("/GroupServlet")
+public class GroupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GroupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/C2/LoginServlet");
+			return;
+		}
+
+		//ユーザーごとに所属しているグループのデータを取得
+		int login = (int)session.getAttribute("user_ID");
+
+		//GroupsDAOに処理してもらう
+		GroupsDAO abc = new GroupsDAO();
+
+		request.setAttribute("user_ID", abc.select (login));
+
+		// グループ一覧ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/group.jsp");
+		dispatcher.forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/C2/LoginServlet");
+			return;
+		}
+
+
+
+
+		doGet(request, response);
+	}
+
+}
