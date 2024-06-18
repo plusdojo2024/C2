d0eab24215d7add75d6bf433469ecbc0aa5fd1d8
plusdojo2024-matsuoka <matsuoka-takumi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 11:24:11 +0900

    Update result.js

--- WebContent/js/result.js
+++ WebContent/js/result.js
@@ -1,8 +1,10 @@
 'use strict';
 
 //登録結果Windowアラート表示
-        if('${result.title}' === '') {
-        }
-        else {
-        	window.alert('${result.message}');
-        }
+if('${result.title}' === '') {
+}
+else {
+	window.alert('${result.message}');
+}
+console.log('${result.title}');
+console.log('${result.message}');
